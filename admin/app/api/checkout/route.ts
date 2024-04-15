b07aa42cca4ef1af96c14aadc45da8fe03a7fd80
avanish7390 <singhavanish7700@gmail.com>
2024-04-15 23:14:19 +0530

    commit

---
+++ admin/app/api/checkout/route.ts
@@ -0,0 +1,57 @@
+import { stripe } from "@/lib/stripe";
+import { NextRequest, NextResponse } from "next/server";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const { cartItems, customer } = await req.json();
+
+    if (!cartItems || !customer) {
+      return new NextResponse("Not enough data to checkout", { status: 400 });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
+      },
+      shipping_options: [
+        { shipping_rate: "shr_1P5QFTSIGYM8WNsFkkWYjwK4" },
+        { shipping_rate: "shr_1P5QGgSIGYM8WNsFnCsDOkRm" },
+      ],
+      line_items: cartItems.map((cartItem: any) => ({
+        price_data: {
+          currency: "INR",
+          product_data: {
+            name: cartItem.item.title,
+            metadata: {
+              productId: cartItem.item._id,
+              ...(cartItem.size && { size: cartItem.size }),
+              ...(cartItem.color && { color: cartItem.color }),
+            },
+          },
+          unit_amount: cartItem.item.price * 100,
+        },
+        quantity: cartItem.quantity,
+      })),
+      client_reference_id: customer.clerkId,
+      success_url: `${process.env.ECOMMERCE_STORE_URL}/payment_success`,
+      cancel_url: `${process.env.ECOMMERCE_STORE_URL}/cart`,
+    });
+
+    return NextResponse.json(session, { headers: corsHeaders });
+  } catch (err) {
+    console.log("[checkout_POST]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
